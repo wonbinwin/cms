2cdb8285f77cfcef4f8ef26c905fd1a3baf9a26f
wonbinwin <youngkf@naver.com>
2025-02-24 09:28:55 +0900

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import StuForm from "./CMS/StuForm.jsx";
 import StuItem from "./CMS/StuItem";
 import { useState, useEffect } from "react";
-import { studentin } from "./database.js";
+
 import "./App.css";
 
 const App = () => {
   // localStorage에서 'users' 데이터를 불러옵니다. 없으면 빈 배열을 기본값으로 사용합니다.
   // const storedUser = JSON.parse(localStorage.getItem("users"));
 
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(
+    JSON.parse(localStorage.getItem("users")) ?? []
+  );
+
+  //! 최초렌더링 시 빈 배열을 부여하면 아래의 아무런 배열이 없는 useEffect에서 users를 빈 배열로 만들어버리는 버그 발생
+  //! 초기값을 로컬스토리지 안의 users를 검사하여 주는 방식으로 변경
+
   useEffect(() => {
     localStorage.setItem("users", JSON.stringify(users));
-    console.log("updated");
   });
 
   useEffect(() => {
